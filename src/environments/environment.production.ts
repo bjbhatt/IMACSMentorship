8247cad943b75bb571fdcd7e44d6a0a15fec0cdf
B J Bhatt <bjbhatt@hotmail.com>
2018-07-23 14:05:29 -0400

    Update IMACMentorship to IMACSMentorship. Add new environments for Development, Test and Production. Remove old prod enviroment.

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://connect.niehs.nih.gov/imacs/rest/loginService.cfc?method='
+};
---
+++ src/environments/environment.test.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://connect3tst.niehs.nih.gov/imacs/rest/loginService.cfc?method='
+};
