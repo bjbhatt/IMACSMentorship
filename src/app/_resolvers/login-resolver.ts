4e3150fd447fb49e0b3e62a7f897d5b07f248130
B J Bhatt <bjbhatt@hotmail.com>
2018-07-26 14:17:55 -0400

    Navigate to Home when logged in only if you are not on HomeComp

--- src/app/_resolvers/login-resolver.ts
+++ src/app/_resolvers/login-resolver.ts
@@ -5,6 +5,8 @@ import { AlertifyService } from '../_services/alertify.service';
 import { ApiService } from '../_services/api.service';
 import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
+import { HomeComponent } from '../home/home.component';
+import { type } from 'os';
 
 @Injectable()
 export class LoginResolver implements Resolve<Login> {
@@ -14,6 +16,9 @@ export class LoginResolver implements Resolve<Login> {
         return this.apiService.loggedIn().pipe(
             catchError(error => {
                 this.alertifyService.error('Problem retrieving data');
+                if (route.routeConfig.component.name !== 'HomeComponent') {
+                    this.router.navigate(['/']);
+                }
                 return of(null);
             })
         );
