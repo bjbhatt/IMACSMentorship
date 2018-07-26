276f1c55d14198e011ae275f17768bd21722b292
Sagar <sathakore@attain.com>
2018-07-26 09:25:15 -0400

    add mentor prompt pages for expertise and location

--- src/app/mentee-prompts/mentee-prompts.component.ts
+++ src/app/mentee-prompts/mentee-prompts.component.ts
@@ -14,6 +14,9 @@ export class MenteePromptsComponent implements OnInit {
   model: MentorOptions;
   isLoggedIn: boolean;
   step = 1;
+  specializations: Map<string, string>;
+  expertises: Map<string, string>;
+  locations: Map<string, string>;
 
   constructor(private apiService: ApiService, private alertifyService: AlertifyService, private router: Router) { }
 
@@ -26,6 +29,36 @@ export class MenteePromptsComponent implements OnInit {
       expertise: [],
       location: [],
     };
+    this.specializations = new Map<string, string>();
+    this.expertises = new Map<string, string>();
+    this.locations = new Map<string, string>();
+    this.specializations
+      .set('dermatomyositis', 'Dermato-myositis')
+      .set('polymyositis', 'Polymyositis')
+      .set('inclusion_body_myositis', 'Inclusion Body Myositis')
+      .set('n/a', 'Doesn\'t Matter');
+    this.expertises
+      .set('epidemiology', 'Epidemiology')
+      .set('disease_assesment', 'Disease Assesment')
+      .set('clinical_trials_therapy', 'Clinical Trials/Therapy')
+      .set('disease_history', 'Disease History')
+      .set('pathology', 'Pathology')
+      .set('outcomes', 'Outcomes')
+      .set('muscle_molecular_biology', 'Muscle Molecular Biology')
+      .set('immunology', 'Immunology');
+    this.locations
+      .set('north_america', 'North America')
+      .set('europe', 'Europe')
+      .set('asia', 'Asia')
+      .set('central_america', 'Central America')
+      .set('south_america', 'South America')
+      .set('africa', 'Africa')
+      .set('australia', 'Australia')
+      .set('n/a', 'Any Location');
+  }
+
+  getKeys(map) {
+    return Array.from(map.keys());
   }
 
   loadModel() {
@@ -43,6 +76,27 @@ export class MenteePromptsComponent implements OnInit {
     this.model.focus = focus;
     console.log(focus);
   }
+
+  // setData(string?: string, array?: string[]) {
+  //   const index = array.indexOf(string, 0);
+  //   if (string === 'n/a') {
+  //     if (index > -1) {
+  //       array = [];
+  //     } else {
+  //       array = [];
+  //       array.push(string);
+  //     }
+  //   } else if (index > -1) {
+  //     array.splice(index, 1);
+  //   } else {
+  //     const index_na = array.indexOf('n/a', 0);
+  //     if (index_na > -1) {
+  //       array.splice(index_na, 1);
+  //     }
+  //     array.push(string);
+  //   }
+  //   console.log(array);
+  // }
 
   setSpecialization(specialization?: string) {
     const index = this.model.specialization.indexOf(specialization, 0);
@@ -65,7 +119,50 @@ export class MenteePromptsComponent implements OnInit {
     console.log(this.model.specialization);
   }
 
+  setExpertise(expertise?: string) {
+    const index = this.model.expertise.indexOf(expertise, 0);
+    if (expertise === 'n/a') {
+      if (index > -1) {
+        this.model.expertise.splice(index, 1);
+      } else {
+        this.model.expertise = [];
+        this.model.expertise.push('n/a');
+      }
+    } else if (index > -1) {
+      this.model.expertise.splice(index, 1);
+    } else {
+      const index_na = this.model.expertise.indexOf('n/a', 0);
+      if (index_na > -1) {
+        this.model.expertise.splice(index_na, 1);
+      }
+      this.model.expertise.push(expertise);
+    }
+    console.log(this.model.expertise);
+  }
+
+  setLocation(location?: string) {
+    const index = this.model.location.indexOf(location, 0);
+    if (location === 'n/a') {
+      if (index > -1) {
+        this.model.location.splice(index, 1);
+      } else {
+        this.model.location = [];
+        this.model.location.push('n/a');
+      }
+    } else if (index > -1) {
+      this.model.location.splice(index, 1);
+    } else {
+      const index_na = this.model.location.indexOf('n/a', 0);
+      if (index_na > -1) {
+        this.model.location.splice(index_na, 1);
+      }
+      this.model.location.push(location);
+    }
+    console.log(this.model.location);
+  }
+
   nextPage() {
+    console.log(this.model);
     if (this.step < 5) {
       this.step++;
     }
@@ -80,7 +177,9 @@ export class MenteePromptsComponent implements OnInit {
   disableNext() {
     if ((this.step === 1 && this.model.clinicalCare == null)
     || (this.step === 2 && this.model.focus == null)
-    || (this.step === 3 && this.model.specialization.length === 0)) {
+    || (this.step === 3 && this.model.specialization.length === 0)
+    || (this.step === 4 && this.model.expertise.length === 0)
+    || (this.step === 5 && this.model.location.length === 0)) {
       return true;
     } else {
       return false;
