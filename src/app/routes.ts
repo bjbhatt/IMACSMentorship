1d6fc9dce71328820b04be974b4cc608df288aec
B J Bhatt <bjbhatt@hotmail.com>
2018-07-28 14:20:50 -0400

    Rename router path requests to mentorReqeusts

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -5,8 +5,6 @@ import { UserProfileComponent } from './user-profile/user-profile.component';
 import { NotLoggedInComponent } from './not-logged-in/not-logged-in.component';
 import { MentorRequestsComponent } from './mentor-requests/mentor-requests.component';
 import { AdministrationComponent } from './administration/administration.component';
-// import { MenteePromptsComponent } from './mentee-prompts/mentee-prompts.component';
-// import { MentorResultsComponent } from './mentor-results/mentor-results.component';
 import { MentorSearchComponent } from './mentor-search/mentor-search.component';
 
 import { LoginResolver } from './_resolvers/login-resolver';
@@ -15,9 +13,7 @@ export const appRoutes: Routes = [
     {path: 'home', component: HomeComponent, resolve: {login: LoginResolver} },
     {path: 'profile', component: UserProfileComponent, resolve: {login: LoginResolver}  },
     {path: 'administration', component: AdministrationComponent, resolve: {login: LoginResolver}  },
-    {path: 'requests', component: MentorRequestsComponent, resolve: {login: LoginResolver} },
-    // {path: 'find', component: MenteePromptsComponent, resolve: {login: LoginResolver} },
-    // {path: 'results', component: MentorResultsComponent, resolve: {login: LoginResolver} },
+    {path: 'mentorRequests', component: MentorRequestsComponent, resolve: {login: LoginResolver} },
     {path: 'mentorSearch', component: MentorSearchComponent, resolve: {login: LoginResolver} },
     {path: 'notLoggedIn', component: NotLoggedInComponent },
     {path: '**', component: HomeComponent, resolve: {login: LoginResolver} },
