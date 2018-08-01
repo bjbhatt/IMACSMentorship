ca2dbd0ea16e3036a96b8dac53efc7adf3404de6
Sagar <sathakore@attain.com>
2018-08-01 12:52:02 -0400

    update file to include future names

--- src/app/_models/mentorship-search-options.ts
+++ src/app/_models/mentorship-search-options.ts
@@ -1,7 +1,7 @@
 export interface MentorSearchOptions {
-    clinicalCare?: string;
-    focus?: string;
-    specialization?: string[];
-    expertise?: string[];
+    clinicalCare?: string; // TBD : New Name : patients
+    focus?: string;  // TBD : New Name : patientType: number
+    specialization?: string[]; // TBD: New Name : clinicalForm: number[]
+    expertise?: string[]; // TBD: New Name : researchArea: number[]
     location?: string[];
 }
